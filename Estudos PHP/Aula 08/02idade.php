34bbd6b9d2e11b4b6917ab1fe22d72d222041e79
Geovani dos Passos <84425834+GeovaniPassos@users.noreply.github.com>
2022-08-29 19:58:05 -0300

    Aula 08 - Integração HTML/PHP

---
+++ Estudos PHP/Aula 08/02idade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../_css/estilo.css">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <?php
+            $nome = isset($_GET["nome"])?$_GET["nome"]:"[Nome nao informado]";
+            $ano = isset($_GET["ano"])?$_GET["nome"]:0;
+            $sexo = isset($_GET["sexo"])?$_GET["sexo"]:"[Sem sexo]";
+            $idade = date ("Y") - $ano;
+            echo "$nome é $sexo, e tem $idade anos."
+        ?>
+        <br>
+        <a href="ex002.html"> VOLTAR </a>
+    </div>
+</body>
+</html>

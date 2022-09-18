9df655f27ab2d6cccfc9501240874eeded56da53
Geovani dos Passos <84425834+GeovaniPassos@users.noreply.github.com>
2022-09-18 11:41:54 -0300

    Count aula 16

---
+++ Estudos PHP/Aula 16/ex004 - strlen.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../_css/estilo03.css">
+    <title>Exercicio de PHP</title>
+</head>
+<body>
+    <div>
+        <?php
+            $txt = "Curso em video";
+            $tam = strlen($txt);
+            echo "O tamanho da string é $tam";
+        ?>
+    </div>
+</body>
+</html>
